591e82ba7f29bb94d987260324813dfa91c1b66a
kasia12345 <katarzyna.rzonca@hotmail.com>
2017-03-19 16:53:52 +0100

    Add files via upload

---
+++ Cwiczenie3/Zad2/Class1Testy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KontoBankowe.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1Testy
+    {
+        [Test]
+        public void Test_Wplacanie()
+        {
+            KontoBankowe source = new KontoBankowe();
+            source.WplacPieniadze(200);
+            Assert.AreEqual(200, source.StanKonta);
+        }
+
+        [Test]
+        public void Test_Wyplacanie()
+        {
+            KontoBankowe source = new KontoBankowe();
+            source.WplacPieniadze(200);
+            source.WyplacPieniadze(20);
+            Assert.AreEqual(180, source.StanKonta);
+        }
+
+        [Test]
+        public void Test_StanKonta()
+        {
+            KontoBankowe source = new KontoBankowe();
+            source.WplacPieniadze(200);
+            Assert.AreEqual(200, source.StanKonta);
+        }
+
+        [Test]
+        public void Test_Transfer()
+        {
+            KontoBankowe source = new KontoBankowe();
+            source.WplacPieniadze(200);
+
+            KontoBankowe destination = new KontoBankowe();
+            destination.WplacPieniadze(150);
+
+            source.TransferPieniedzy(source, destination, 100);
+
+            Assert.AreEqual(250, destination.StanKonta);
+            Assert.AreEqual(100, source.StanKonta);
+        }
+    }
+}
